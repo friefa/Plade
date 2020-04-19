b2cf55d2870ac85c3a546e9a27065f0017689877
Fabian Friedl <friedlfabian@gmx.de>
2020-04-19 19:48:42 +0200

    [BB-2] Added local dependency solver

--- lib/module/dependency/moduleDependencySolver.php
+++ lib/module/dependency/moduleDependencySolver.php
@@ -1,7 +1,22 @@
 <?php
+/**
+ * BABOOK
+ * ModuleDependencySolver
+ * Fabian Friedl
+ * 19.04.2020
+ * All rights reserved.
+ */
 
+
+/**
+ * This object resolves dependencies between modules.
+ */
 class ModuleDependencySolver
 {
+    /**
+     * This function resolves all dependencies of all modules.
+     * If a dependency could not be resolved, an error is displayed.
+     */
     public function Solve(array $modules) : string
     {
         $result = "";
@@ -27,6 +42,38 @@ class ModuleDependencySolver
                 {
                     print("[MODULE-DEPENDENCY-SOLVER] Failed to solve dependency '".$dependency->FileName."'!");
                 }
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * This function resolves all dependencies belonging to the module.
+     * If a dependency could not be resolved, the function returns an error.
+     */
+    public function SolveLocal(object $targetModule, array $modules) : string 
+    {
+        $result = "";
+        $addedDependencies = array();
+
+        foreach ($modules as $module)
+        {
+            foreach ($module->ModuleConfig->Dependencies as $dependency)
+            {
+                if (!in_array($dependency->FileName, $addedDependencies) && $dependency->OwnedBy($module->ModuleConfig->Name) && $targetModule->ModuleConfig->HasDependency($dependency))
+                {
+                    $result .= ModuleDependencyType::GetDependencyString($dependency->Type, $module->ModuleConfig->Name, $dependency->FileName) . PHP_EOL;
+                    $addedDependencies[] = $dependency->FileName;
+                }
+            }
+        }
+        
+        foreach ($targetModule->ModuleConfig->Dependencies as $dependency)
+        {
+            if (!in_array($dependency->FileName, $addedDependencies))
+            {
+                print("[MODULE-DEPENDENCY-SOLVER] Failed to solve local dependency '".$dependency->FileName."'!");
             }
         }
 
