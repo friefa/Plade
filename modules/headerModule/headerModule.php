1f00a55efb7d8b5f790a990ecd4e1d0e6a0aba27
Fabian Friedl <friedlfabian@gmx.de>
2020-04-19 20:56:18 +0200

    [BB-2] Added all relevant comments to all files

--- modules/headerModule/headerModule.php
+++ modules/headerModule/headerModule.php
@@ -1,9 +1,23 @@
 <?php
+/**
+ * BABOOK
+ * HeaderModule
+ * Fabian Friedl
+ * 19.04.2020
+ * All rights reserved.
+ */
 
+// Implementations
 include_once("lib/utils/stringUtils.php");
 
+/**
+ * This object is a module that implements the header and all dependencies.
+ */
 class HeaderModule extends Module
 {
+    /**
+     * This method displays the module on the page.
+     */
     function render(array $params)
     {
         $template = file_get_contents("modules/headerModule/templates/header.html");
