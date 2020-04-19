2a7035498b7b6d9c8ae75d86a8bb54aba9626792
Fabian Friedl <friedlfabian@gmx.de>
2020-04-19 11:45:56 +0200

    [BB-1] Replacement injection added to parent class of module

--- lib/module/module.php
+++ lib/module/module.php
@@ -4,6 +4,19 @@ abstract class Module {
     public $ModuleConfig;
 
     abstract protected function render(array $params);
+
+    protected function InsertReplacements(string &$template, array $params) : string
+    {
+        foreach ($params as $key => $value)
+        {
+            if (StringUtils::StartsWith($key, "%") && StringUtils::EndsWith($key, "%"))
+            {
+                $template = str_replace($key, $value, $template);
+            }
+        }
+
+        return $template;
+    }
 }
 
 ?>
