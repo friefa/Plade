50afaf62d2c6fb5e925c761f84814a90201467c0
Fabian Friedl <friedlfabian@gmx.de>
2020-04-23 21:22:44 +0200

    [BB-12] Added application configuration and initialization

--- default.php
+++ default.php
@@ -8,9 +8,13 @@
  */
 
 // Implementations
+include_once("lib/engine/applicationConfig.php");
 include_once("lib/module/module.php");
 include_once("lib/module/moduleLoader.php");
 include_once("lib/module/moduleHandler.php");
+
+// Initialize the application configuration
+ApplicationConfig::Init();
 
 // Module Handler is started and manages all modules and the display of the web application.
 $params = array_merge($_GET, $_POST);
@@ -19,6 +23,8 @@ if (!isset($params['module']))
 {
     $params['module'] = 'login';
 }
+
+
 
 $moduleHandler = new ModuleHandler();
 $moduleHandler->Handle($params);
---
+++ lib/engine/applicationConfig.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * BABOOK
+ * ApplicationConfig
+ * Fabian Friedl
+ * 23.04.2020
+ * All rights reserved.
+ */
+
+/**
+ * This class globally represents all application settings of the engine.
+ */
+class ApplicationConfig
+{
+    // Data fields
+    public const CONFIG_FILE = "appconfig.json";
+    public static string $EngineVersion;
+    public static string $EntryModule;
+    public static bool $DebugMode;
+    public static array $Replacements;
+
+    /**
+     * This static method is used to load and initialize the application configuration.
+     */
+    public static function Init() : void
+    {
+        if (file_exists(self::CONFIG_FILE))
+        {
+            $raw = file_get_contents(self::CONFIG_FILE);
+            $json = json_decode($raw, true);
+
+            if (self::ValidateJsonArray($json))
+            {
+                self::$EngineVersion = $json['EngineVersion'];
+                self::$EntryModule = $json['EngineVersion'];
+                self::$DebugMode = filter_var($json['EngineVersion'], FILTER_VALIDATE_BOOLEAN);
+                self::$Replacements = $json['Replacements'];
+            }
+            else
+            {
+                print("[APPLICATION-CONFIG] Configuration file is invalid!");
+            }
+        }
+        else
+        {
+            print("[APPLICATION-CONFIG] Configuration file was not found!");
+        }
+    }
+
+    /**
+     * This private static method checks if the given array is a valid configuration.
+     */
+    private static function ValidateJsonArray(array $arr) : bool
+    {
+        return isset($arr['EngineVersion'], $arr['EntryModule'], $arr['DebugMode'], $arr['Replacements']);
+    }
+}
+
+?>
